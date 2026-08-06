95052b0a493716bc1e1e5ad018d996c7560db444
Yash_Chandiramani <chandiramaniyash@gmail.com>
2026-08-06 10:31:11 +0530

    edited the test 2.js file

--- test2.js
+++ test2.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+let passed = true;
+console.log("regsitration test");
 if(fs.existSync("registration.html")){
     console.log("TC-01 : registration.html exists : PASS");
 }
